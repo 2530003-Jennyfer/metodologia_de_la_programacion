188e8c0e17dd4c30a342a890ffdd629eec2d4c35
jennyfer Rodriguez <7rodriguezjennyfer@gmail.com>
2025-11-03 10:40:06 -0600

    numbers

---
+++ src/understading_numbers.py
@@ -0,0 +1,50 @@
+# Numbers
+"""
+Enteros
+   Los podemos sumar (+), restar (-),
+   multiplicar (*) y dividir (/).
+   Les podemos sacar la ptencias (**2, **3, **4, ...),
+   modulo (%).
+
+"""
+number_1 = 35
+number_2 = 35
+suma = number_1+number_2
+resta = number_1-number_2
+multiplicacion = number_1*number_2
+division = number_1/number_2
+potencia  = number_1**2
+modulo = number_1%number_2
+print("suma: ", suma, type(suma))
+print("resta: ", resta, type(resta))
+print("multiplicacion: ", multiplicacion, type(multiplicacion))
+print("division: ", division, type(division))
+print("potencia: ",potencia , type (potencia))
+print("modulo: ", modulo, type(modulo))
+"""
+Jerarquía de operaciones
+2+3*4 = 14
+(2+3)*4= 20
+"""
+"""
+floats 
+Python llama Floast a cualquier número con un punto decimal. 
+   Los podemos sumar (+), restar (-),
+   multiplicar (*) y dividir (/).
+   Les podemos sacar la ptencias (**2, **3, **4, ...),
+   modulo (%).
+
+"""
+print(0.1+0.1)
+print(0.2-0.2)
+print(0.1*0.2)
+print(2*0.2)
+# Imprimir la edad de alguien 
+age = 33
+message = "Charly tiene " + str(age) + " años. "
+print(message)
+"""
+TypeError: Esto significa que Python no pude 
+reconocer el tipo de información 
+que se está utilizando. 
+"""
