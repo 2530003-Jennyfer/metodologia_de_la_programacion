c27b57607a59015715f21d31d17c04e797b4b8c0
jennyfer Rodriguez <7rodriguezjennyfer@gmail.com>
2025-11-13 05:53:49 -0600

    Las tuplas

---
+++ src/understading_tuples.py
@@ -0,0 +1,20 @@
+# Tuples
+"""
+ Las tuplas son listas de elementos que no 
+ cambian de tamaño. Las tuplas son INMUTABLES.
+
+ Se utilizan los () para definir una tupla.
+"""
+rectangle = (200, 50) #(largo, ancho)
+print(rectangle[0])
+print(rectangle[1])
+#for mesure in rectangle_mesurement:
+   # print(mesure)
+#print(dir(rectangle_mesurement))# built in dir
+ # Regresando a las listas 
+cars=["bwm","porche","masda"]
+print(cars)
+cars[0]="bmw"
+cars[1]="porshe"
+cars[2]="Mazda"
+print(cars)
