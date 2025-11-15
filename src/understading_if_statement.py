90c6aca9234e09a9f957febd3601914b6130457c
jennyfer Rodriguez <7rodriguezjennyfer@gmail.com>
2025-11-15 09:33:23 -0600

    If statment

--- src/understading_if_statement.py
+++ src/understading_if_statement.py
@@ -5,11 +5,11 @@ for car in cars:
     else:
         print(car)
 
-# Condicionales: El condicional es el corazón de un if
+
 # Condicional True
 car = "bmw"
 print(car=="bmw") #Salida-> true
-# Condicional False
+# Condicional False solucion a la condicion falsa
 car_2= 'Audi'
 print(car_2=='audi')#Salida->False
 #Condicional True
@@ -20,6 +20,7 @@ requested_tooping = "mushrooms" #->String
 if requested_tooping != 'anchovies': # ->True
     print("Hold the anchovies")
 # Comparaciones númericas 
+# Condicionales numéricos 
 age =18 #->int
 print(age==18)#->true
 answer =17
@@ -30,4 +31,47 @@ print(age<21)#True
 print(age<=21)#True
 print(age>21)#False
 print(age>=21)#False
-# Multiple adicional 
+# Multiple condiciones 
+age_0 = 22
+age_1 =18
+print("Multiple condiciones")
+print("Operaciones and - pseint (O)")
+print(age_0 >= 21 and age_1 >= 21)
+print(age_0 >= 21 and age_1 >= 18)
+age_0 = 22
+age_1 =18
+print("Multiple condiciones")
+print("Operaciones or - pseint (O)")
+print(age_0 >= 21 or age_1 >= 21)
+print(age_0 >= 23 or age_1 >= 21)
+# ¿Como nos preguntamos si algun valor está en una lista? 
+print("\n A value is in a lista")
+requested_toppings = ['mushrooms', 'onions', 'pineapple']
+print('mushrooms' in requested_toppings)
+print('pepperoni' in requested_toppings)
+# A value not in a list
+banned_users = ["gabriel", 'max', ' adnrik', 'quevedo', 'christo']
+user = "pedro"
+print(user not in banned_users)
+# Variables de tipo booleanos 
+#game_ active =True 
+#can_edit =False
+
+"""
+ If statment
+ if condition: 
+    do something 
+if condition: 
+    do something (true)
+else: 
+    do something (False)
+
+"""
+# Preguntar la edad del user y decirle si tiene la edad suficiente para votar
+# input()
+age = int(input("\nCual es tu edad: "))
+print(f"\nTu edad es: {age}")
+if (age) >=18:
+    print("Tu tienes la edad para votar")
+else:
+    print("Lo siento, eres demasiado joven para votar")
